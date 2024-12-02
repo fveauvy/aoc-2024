aa52a1ba111d3f52ce1678b3ab41c6aebd6638f1
François VEAUVY <veauvy.fr@gmail.com>
2024-12-02 23:31:18 +0100

    update setup-day for day 03

--- src/day-03/day-03.test.ts
+++ src/day-03/day-03.test.ts
@@ -1,29 +1,22 @@
 import { expect, test } from "bun:test";
-import { loadInput } from "../utils";
 import { solvePart1, solvePart2 } from "./day-03";
 
-const textFileUrl = new URL(
+const TEXT_FILE_URL_EXAMPLE = new URL(
 	import.meta.url.replace(".test.ts", ".example.txt"),
 );
 
 test("Day 03 - Part 1", () => {
-	// Load input
-	const input = loadInput(textFileUrl);
-
 	// Expected Value
 	const expectedValue = 0;
 
 	// Evaluation
-	expect(solvePart1(input)).toBe(expectedValue);
+	expect(solvePart1(TEXT_FILE_URL_EXAMPLE)).toBe(expectedValue);
 });
 
 test("Day 03 - Part 2", () => {
-	// Load input
-	const input = loadInput(textFileUrl);
-
 	// Expected Value
 	const expectedValue = 0;
 
 	// Evaluation
-	expect(solvePart2(input)).toBe(expectedValue);
+	expect(solvePart2(TEXT_FILE_URL_EXAMPLE)).toBe(expectedValue);
 });
--- src/day-03/day-03.ts
+++ src/day-03/day-03.ts
@@ -1,26 +1,34 @@
 import { loadInput, parseLines } from "../utils";
 
-const textFileUrl = new URL(import.meta.url.replace(".ts", ".txt"));
+const TEXT_FILE_URL = new URL(import.meta.url.replace(".ts", ".txt"));
 
-// Load input
-const input = loadInput(textFileUrl);
-const lines = parseLines(input);
+export function loadAndParse(textFileUrl: URL): string[] {
+	const input = loadInput(textFileUrl);
+	const lines = parseLines(input);
+	return lines;
+}
 
 // Implement Part 1 logic
-export function solvePart1(data: string[]): number {
+export function solvePart1(textFileUrl: URL): number {
+	// Load and parse the input
+	const lines = loadAndParse(textFileUrl);
+
 	const result = 0;
 	console.log("Result:", result);
 	return result;
 }
 
 // Implement Part 2 logic
-export function solvePart2(data: string[]): number {
+export function solvePart2(textFileUrl: URL): number {
+	// Load and parse the input
+	const lines = loadAndParse(textFileUrl);
+
 	const result = 0;
 	console.log("Result:", result);
 	return result;
 }
 
 if (import.meta.main) {
-	solvePart1(lines);
-	solvePart2(lines);
+	solvePart1(TEXT_FILE_URL);
+	solvePart2(TEXT_FILE_URL);
 }
