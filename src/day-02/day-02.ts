ebbe7d37ad2c7763b8e3c9a436bef42a0ca8682d
François VEAUVY <veauvy.fr@gmail.com>
2024-12-02 20:13:47 +0100

    solve part 2

--- src/day-02/day-02.ts
+++ src/day-02/day-02.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { loadInput, parseLines } from "../utils";
+import { debugLog, loadInput, parseLines } from "../utils";
 
 type Report = number[];
 const textFileUrl = new URL(import.meta.url.replace(".ts", ".txt"));
@@ -14,6 +14,43 @@ export function parseReports(lines: string[]): Report[] {
 	);
 }
 
+type ReportStatus =
+	| {
+			safe: true;
+	  }
+	| { safe: false; levelOnError: number };
+
+function isReportSafe(report: Report): ReportStatus {
+	const firstSub = report[0] - report[1];
+	if (firstSub === 0 || Math.abs(firstSub) > 3) {
+		// This is an unsafe report from the start, we skip it
+		return { safe: false, levelOnError: 1 };
+	}
+	const orderBy: "asc" | "desc" = firstSub > 0 ? "desc" : "asc";
+
+	for (let i = 1; i < report.length - 1; i++) {
+		const sub = report[i] - report[i + 1];
+
+		// Any two adjacent levels differ by at least one and at most three
+		if (sub === 0 || Math.abs(sub) > 3) {
+			return { safe: false, levelOnError: i + 1 };
+		}
+
+		// The levels are either all increasing or all decreasing
+		if ((orderBy === "desc" && sub < 0) || (orderBy === "asc" && sub > 0)) {
+			return { safe: false, levelOnError: i + 1 };
+		}
+
+		// Penultimate level has been reached and the report is safe
+		if (i === report.length - 2) {
+			return { safe: true };
+		}
+	}
+	throw new Error(
+		`The loop ended unexpectedly\nReport was ${report.toString()}`,
+	);
+}
+
 // Load input
 const input = loadInput(textFileUrl);
 const lines = parseLines(input);
@@ -24,31 +61,10 @@ export function solvePart1(data: Report[]): number {
 	let safeReportCount = 0;
 
 	for (const report of data) {
-		const firstSub = report[0] - report[1];
-		if (firstSub === 0 || Math.abs(firstSub) > 3) {
-			// This is an unsafe report from the start, we skip it
-			continue;
-		}
-		const orderBy: "asc" | "desc" = firstSub > 0 ? "desc" : "asc";
-
-		for (let i = 1; i < report.length - 1; i++) {
-			const sub = report[i] - report[i + 1];
-
-			// Any two adjacent levels differ by at least one and at most three
-			if (sub === 0 || Math.abs(sub) > 3) {
-				break;
-			}
-
-			// The levels are either all increasing or all decreasing
-			if ((orderBy === "desc" && sub < 0) || (orderBy === "asc" && sub > 0)) {
-				break;
-			}
-
-			// Penultimate level has been reached and the report is safe
-			if (i === report.length - 2) {
-				safeReportCount++;
-				// console.log("This report is safe: %s", report);
-			}
+		const status = isReportSafe(report);
+		if (status.safe) {
+			safeReportCount++;
+			debugLog("This report is safe: %s", report);
 		}
 	}
 
@@ -56,14 +72,70 @@ export function solvePart1(data: Report[]): number {
 	return safeReportCount;
 }
 
-export function solvePart2(data: string[]): number {
+export function solvePart2(data: Report[]): number {
 	// Implement Part 2 logic
-	const result = 0;
-	console.log("Result:", result);
-	return result;
+	let safeReportCount = 0;
+
+	for (const report of data) {
+		const status = isReportSafe(report);
+		if (status.safe) {
+			safeReportCount++;
+			debugLog("This report is safe: %s", report);
+		} else {
+			const reportDampenerApplied = report.toSpliced(status.levelOnError, 1);
+
+			const statusWithDampener = isReportSafe(reportDampenerApplied);
+
+			if (statusWithDampener.safe) {
+				safeReportCount++;
+				debugLog(
+					"Initially %s - removed index %d - report is now safe: %s",
+					report,
+					status.levelOnError,
+					reportDampenerApplied,
+				);
+				continue;
+			}
+			const reportDampenerAppliedPrev = report.toSpliced(
+				status.levelOnError - 1,
+				1,
+			);
+
+			const statusWithDampenerPrev = isReportSafe(reportDampenerAppliedPrev);
+			if (statusWithDampenerPrev.safe) {
+				safeReportCount++;
+				debugLog(
+					"Initially %s - removed index %d - report is now safe: %s",
+					report,
+					status.levelOnError - 1,
+					reportDampenerAppliedPrev,
+				);
+				continue;
+			}
+
+			const reportDampenerAppliedFirst = report.toSpliced(0, 1);
+			const statusWithDampenerFirst = isReportSafe(reportDampenerAppliedFirst);
+			if (statusWithDampenerFirst.safe) {
+				safeReportCount++;
+				debugLog(
+					"Initially %s - removed index %d - report is now safe: %s",
+					report,
+					0,
+					reportDampenerAppliedFirst,
+				);
+			}
+			debugLog("This report is not safe: %s", report);
+		}
+	}
+
+	console.log(
+		"Count of safe Reports (with the Problem Dampener):",
+		safeReportCount,
+	);
+	return safeReportCount;
 }
 
 if (import.meta.main) {
 	solvePart1(reports);
-	// solvePart2(lines);
+	solvePart2(reports);
 }
